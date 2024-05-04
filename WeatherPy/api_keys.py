9a1f63f3d5a2cfe8150c975dc509088d1f615290
Stephen <stephen.loucel@gmail.com>
2024-05-04 16:23:39 -0700

    .

--- WeatherPy/api_keys.py
+++ WeatherPy/api_keys.py
@@ -1,5 +1,5 @@
 # OpenWeatherMap API Key
-weather_api_key = "YOUR KEY HERE"
+weather_api_key = "cffec7e04a0433ade332112f1281f1d9"
 
 # Geoapify API Key
-geoapify_key = "YOUR KEY HERE"
+geoapify_key = "a03d6926da2e42ed903565d70a851fc8"
